8ac4df56bc98b14bcad1d7d1928b4f5c720eec68
Andrey Golovkov <mail.agolovkov@gmail.com>
2020-10-15 00:20:22 +0300

    Implementation of dynamic styling applied to components,size property is taken from property at state.
    <div
      style={{
        backgroundImage: `url(${imageUrl})`
      }}
      className={`${size} menu-item`}>
    .....
    </div>
    
    Changes to be committed:
    	modified:   src/components/directory/directory.component.jsx
    	modified:   src/components/menu-item/menu-item.component.jsx
    	modified:   src/components/menu-item/menu-item.styles.scss
    	modified:   src/pages/homepage/homepage.component.jsx
    	modified:   src/pages/homepage/homepage.styles.scss

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -6,53 +6,12 @@ import MenuItem from '../../components/menu-item/menu-item.component';
 
 const HomePage = () => (
   <div className='homepage'>
-    
-  <Directory>
-    <MenuItem />
-  
-  </Directory>
 
-  
-    <div className="directory-menu">
+    <Directory>
+      <MenuItem />
+    </Directory>
 
-      <div className="menu-item"> 
-        <div className="content">
-          <h1 className="title">HATS</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-    
-      <div className="menu-item"> 
-        <div className="content">
-          <h1 className="title">JACKETS</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-    
-      <div className="menu-item"> 
-        <div className="content">
-          <h1 className="title">SNEAKERS</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-    
-      <div className="menu-item"> 
-        <div className="content">
-          <h1 className="title">WOMENs</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-    
-      <div className="menu-item"> 
-        <div className="content">
-          <h1 className="title">MENs</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-    
-    
-      </div>
-  </div>
+    </div>
 )
 
 
