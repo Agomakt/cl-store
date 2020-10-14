058bdd35181d1512387c9ece6fbeccb99e0c142f
Andrey Golovkov <mail.agolovkov@gmail.com>
2020-10-15 00:03:48 +0300

    Html layout code is converted into components Created statefull Class component with constructor() Data imported into components over the state
    On branch html-wireframing
    Your branch is up to date with 'origin/html-wireframing'.
    
    Changes to be committed:
    	modified:   src/App.js
    	new file:   src/components/directory/directory.component.jsx
    	new file:   src/components/directory/directory.styles.scss
    	new file:   src/components/menu-item/menu-item.component.jsx
    	renamed:    src/homepage.styles.scss -> src/components/menu-item/menu-item.styles.scss
    	renamed:    src/homepage.component.jsx -> src/pages/homepage/homepage.component.jsx
    	new file:   src/pages/homepage/homepage.styles.scss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import HomePage from './homepage.component'
+import HomePage from './pages/homepage/homepage.component'
 import './App.css';
 
 
 function App() {
   return (
-    <HomePage />
+    <div>
+      <HomePage />
+    
+    </div>
   );
 }
 
