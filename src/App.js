a69b60e82087773e6821ffa3723655547fc017f9
Andrey Golovkov <mail.agolovkov@gmail.com>
2020-10-15 13:35:01 +0300

    App navigation with using Link component
    import {Link} from 'react-router-dom';
    
    <Link to="/hats">Choose Hats</Link>
    
    	modified:   src/App.js
    	modified:   src/pages/homepage/homepage.component.jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Link} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component'
 import './App.css';
 
 // create components for handling with Route
 const HatsPage = () => (
   <div>
+    <Link to='/'>HomePage</Link>
     <h1>HATS PAGE</h1>
   </div>  
 );
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,17 +2,20 @@ import React from 'react';
 import './homepage.styles.scss';
 import Directory from '../../components/directory/directory.component';
 import MenuItem from '../../components/menu-item/menu-item.component';
+import {Link} from 'react-router-dom';
 
 
-const HomePage = () => (
+const HomePage = (props) => {
+  console.log(props)
+  return (
   <div className='homepage'>
-
+    <Link to="/hats">Choose Hats</Link>
     <Directory>
       <MenuItem />
     </Directory>
 
     </div>
-)
+)}
 
 
 export default HomePage;
