4db95e86980a7198d6964e90208db50f7a8c4b75
Andrey Golovkov <mail.agolovkov@gmail.com>
2020-10-15 11:00:16 +0300

    Browser-router-dom installed   npm install react-router-dom Implemented as HOC for App component at index.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component'
 import './App.css';
+
+// create components for handling with Route
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>  
+);
+
 
 
 function App() {
