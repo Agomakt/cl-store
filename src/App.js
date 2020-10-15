e59438a92a6c48326b226d8ec409a049d457ef05
Andrey Golovkov <mail.agolovkov@gmail.com>
2020-10-15 12:44:10 +0300

    Switch component used to wrap Route components
    Switch takes the first path which is accessible on the list of
    Route components. That why we need to use exact attribute too.
    
    Changes to be committed:
    	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+import {Switch, Route} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component'
 import './App.css';
 
@@ -16,8 +16,10 @@ function App() {
   
   return (
     <div>
-      <Route exact path='/' component={HomePage}/>
-      <Route path='/hats' component={HatsPage}/>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/hats' component={HatsPage}/>
+      </Switch>
     </div>
   );
 }
