978b815ba7431e834dcbb27a802cd701f64ec4ca
Andrey Golovkov <mail.agolovkov@gmail.com>
2020-10-18 16:35:02 +0300

    home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import {Switch, Route, Link} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component'
 import './App.css';
 
+// create components for handling with Route
+const HatsPage = () => (
+  <div>
+    <Link to='/'>HomePage</Link>
+    <h1>HATS PAGE</h1>
+  </div>  
+);
+
+
+// <HomePage />
 
 function App() {
+  
   return (
     <div>
-      <HomePage />
-    
+      
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/hats' component={HatsPage}/>
+      </Switch>
     </div>
   );
 }
