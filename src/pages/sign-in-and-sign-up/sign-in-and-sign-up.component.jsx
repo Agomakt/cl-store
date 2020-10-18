e13d9daaaf56a07bf2a3b0ba832a347772c98ff5
Andrey Golovkov <mail.agolovkov@gmail.com>
2020-10-18 20:28:00 +0300

    sign-in component added to sign-in-gign-up page

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import './sign-in-and-sign-up.styles.scss'
+import './sign-in-and-sign-up.styles.scss';
+import SignIn from '../../components/sign-in/signin.component';
 
 const SignInAndSignUpPage = () => (
-  <div className='sign-in-and-sign-up'>SIGN IN</div>
+  <div className='sign-in-and-sign-up'>
+    <SignIn />
+  </div>
 
 )
 
