6caa36d6a90adf2a8ec4b1a7e9bb2a2e72907853
Andrey Golovkov <mail.agolovkov@gmail.com>
2020-10-20 17:44:48 +0300

    Google SignIn Authentication implemented to Signin Page

--- src/components/sign-in/signin.component.jsx
+++ src/components/sign-in/signin.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './signin.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from  '../custom-button/custom-button.component';
+import { signInWithGoogle } from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -52,6 +53,10 @@ class SignIn extends React.Component {
           
 
           <CustomButton type="submit"> SIGN IN</CustomButton>
+          <CustomButton onClick={signInWithGoogle}> 
+            {' '}
+            Sign in with Google {' '}
+          </CustomButton>
         </form>
 
       </div>
