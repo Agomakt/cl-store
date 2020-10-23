a440b8156dbe626d1c5dc7916090856c5ee1f167
Andrey Golovkov <mail.agolovkov@gmail.com>
2020-10-23 22:58:37 +0300

    added addItem action at cartReducer

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -2,7 +2,8 @@ import CartActionTypes from './cart.types';
 
 
 const INITIAL_STATE = {
-  hidden: true
+  hidden: true,
+  cartItems: []
 };
 
 
@@ -15,6 +16,11 @@ const cartReducer = (state=INITIAL_STATE, action) => {
         hidden: !state.hidden
       };
       
+    case CartActionTypes.ADD_ITEM:
+        return {
+          ...state,
+          cartItems: [...state.cartItems, action.payload]
+        }
   
     default:
       return state
