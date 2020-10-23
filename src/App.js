2ce061461b669d724b7fb409a24c4f8c64be22c8
Andrey Golovkov <mail.agolovkov@gmail.com>
2020-10-23 14:49:02 +0300

    User Redirect to HomePage if userd signed in. User Action Type is set

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import { connect } from 'react-redux';
 import './App.css';
 
@@ -60,7 +60,14 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route path='/shop' component={ShopPage}/>
-          <Route path='/signin' component={SignInAndSignUpPage}/>
+          <Route 
+            exact path='/signin' 
+            render={() => this.props.currentUser ? (
+              <Redirect to='/'/>
+              ) : (<SignInAndSignUpPage/>
+                )
+              }  
+          />
         </Switch>
       </div>
     );
@@ -69,9 +76,15 @@ class App extends React.Component {
 
 }
 
+// get the currentUser from Redux State
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
+
 
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect( null, mapDispatchToProps )(App);
+export default connect( mapStateToProps, mapDispatchToProps )(App);
