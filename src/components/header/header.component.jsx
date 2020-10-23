ea87deb4d1edb34d1f52b11754d3900eb82e3879
Andrey Golovkov <mail.agolovkov@gmail.com>
2020-10-23 21:59:35 +0300

    Cart reducer implemented into header component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,7 +8,7 @@ import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo'/>
@@ -29,14 +29,15 @@ const Header = ({ currentUser }) => (
       <CartIcon />
     </div>
 
-    <CartDropdown />
+    { hidden ? null : <CartDropdown /> }
 
   </div>
 )
 
 // function that allows access to state, being RootReducer
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
+  currentUser,
+  hidden
 })
 
 
