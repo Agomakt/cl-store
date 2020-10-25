d238e9c653e21bd3aad78ca1d54ca1230ecfa9af
Andrey Golovkov <mail.agolovkov@gmail.com>
2020-10-25 10:14:18 +0300

    updating a total value of cartItcon component with Selectors in Redux

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,10 +5,10 @@ import './cart-icon.styles.scss';
 import { connect } from 'react-redux';
 
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({toggleCartHidden, itemCount}) => (
   <div className='cart-icon' onClick={toggleCartHidden}>
     <ShoppingIcon className='shopping-icon'/>
-    <span className='item-count'>0</span>  
+    <span className='item-count'>{itemCount}</span>  
   </div>
 )
 
@@ -17,5 +17,9 @@ const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
+const mapStateToProps = ({ cart: {cartItems} }) => ({
+  itemCount: cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
+})
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
