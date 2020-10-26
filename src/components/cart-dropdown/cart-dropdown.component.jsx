b94e4f6e8fd98da242adcf5cf222eab2beabc3fb
Andrey Golovkov <mail.agolovkov@gmail.com>
2020-10-26 13:19:16 +0300

    Conditionally render empty span message  at CartDropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -14,9 +14,13 @@ const CartDropdown = ({ cartItems }) => (
   <div className='cart-dropdown'>
   <div className='cart-items'> 
   
-    {cartItems.map(cartItem => (
-      <CartItem key={cartItem.id} item={cartItem} />
-    ))}
+    { 
+      cartItems.lenght ?
+      cartItems.map(cartItem => (
+      <CartItem key={cartItem.id} item={cartItem} /> 
+      ))
+      : <span className='empty-message'> Your cart is empty </span>
+    }
        
     </div>
 
