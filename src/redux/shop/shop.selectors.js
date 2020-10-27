ceb8ac00e359b43bde28279c3b1d9afe7bb35979
Andrey Golovkov <mail.agolovkov@gmail.com>
2020-10-27 13:07:24 +0300

    Built Collection Selector (with COLLECTION_ID_MAP) to reach into Redux state of all collection items and get collection based on match.params.collectionId

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,8 +1,27 @@
 import { createSelector } from 'reselect';
 
+const COLLECTIION_ID_MAP = {
+  // object that maps id to the string value
+  hats: 1,
+  sneakers: 2,
+  jackets: 3,
+  womens: 4,
+  mens: 5
+} 
+
+
 const selectShop = state =>  state.shop;
 
 export const selectCollections = createSelector(
   [selectShop],
   shop => shop.collections
+)
+
+
+export const selectCollection = collectionUrlParam => (
+  createSelector(
+    [selectCollections],
+    collections => 
+      collections.find(collection => collection.id === COLLECTIION_ID_MAP[collectionUrlParam])
+  )
 )
