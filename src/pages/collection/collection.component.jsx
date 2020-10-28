f181bdcde13e141c2bb8a34fb7c67296f64367f7
Andrey Golovkov <mail.agolovkov@gmail.com>
2020-10-28 13:08:15 +0300

    collection page filled with collection items

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -4,17 +4,19 @@ import { connect } from 'react-redux';
 import './collection.styles.scss';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
-
+import CollecttionPreview from '../../components/collection-preview/collection-preview.component';
 import { selectCollection } from '../../redux/shop/shop.selectors';
 
 const CollectionPage = ({collection}) => {
-  // console.log(match)
-  // console.log(match.params.collectionId)
-  //
-  console.log(collection)
+  const {title, items} = collection;
   return (
   <div className='collection-page'> 
-    <h2>COLLECTION PAGE</h2>
+    <h2 className='title'>{title}</h2>
+    <div className='items'>
+      {
+        items.map(item => <CollectionItem key={item.id} item={item}/>)
+      }
+    </div>
   </div>
 );
 }
